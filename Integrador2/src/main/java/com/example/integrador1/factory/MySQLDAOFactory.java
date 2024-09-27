98c37d902701149870933f6934481d3b6793059f
joelviviant <joelviviant31@gmail.com>
2024-09-27 20:26:36 -0300

    int 2

---
+++ Integrador2/src/main/java/com/example/integrador1/factory/MySQLDAOFactory.java
@@ -0,0 +1,63 @@
+package com.example.integrador1.factory;
+
+
+
+import com.example.integrador1.dao.ClienteDAO;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLDAOFactory extends AbstractFactory {
+    private static MySQLDAOFactory instance = null;
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String uri = "jdbc:mysql://localhost:3306/integrador1";
+    public static Connection conn;
+
+    private MySQLDAOFactory() {
+    }
+
+    public static synchronized MySQLDAOFactory getInstance() {
+        if (instance == null) {
+            instance = new MySQLDAOFactory();
+        }
+        return instance;
+    }
+
+    public static Connection createConnection() {
+        if (conn != null) {
+            return conn;
+        }
+        String driver = DRIVER;
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                 | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            conn = DriverManager.getConnection(uri, "root", "");
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ClienteDAO getClienteDAO() {
+        return null;
+    }
+}
